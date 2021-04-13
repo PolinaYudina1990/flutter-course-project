03d7634745bad347bad6c01f991e95ce8af570eb
PolinaYudina1990 <wispy@list.ru>
2021-04-13 16:34:58 +0300

    ex-4.10 image of description

--- lib/domain/details.dart
+++ lib/domain/details.dart
@@ -1,10 +1,12 @@
 class SightDetails {
   final String name;
+  final String url;
   final String type;
   final String workHours;
   final String details;
   SightDetails({
     this.name,
+    this.url,
     this.type,
     this.workHours,
     this.details,
